d27d15c57e94e8e59be1364bf6197b91d67c080b
Xevor123 <108003219+Xevor123@users.noreply.github.com>
2023-09-10 12:21:28 +0200

    Continued Comm Chat view layout. added comment fragment and added function to the create post button

--- app/src/main/java/com/example/gracewellchurchbotnav/ui/CommunityChat/CommChatComment.java
+++
@@ -1,4 +0,0 @@
-package com.example.gracewellchurchbotnav.ui.CommunityChat;
-
-public class CommChatComment {
-}
--- app/src/main/java/com/example/gracewellchurchbotnav/ui/CommunityChat/CommunityChatActivity.java
+++ app/src/main/java/com/example/gracewellchurchbotnav/ui/CommunityChat/CommunityChatActivity.java
@@ -3,7 +3,10 @@ package com.example.gracewellchurchbotnav.ui.CommunityChat;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.gracewellchurchbotnav.R;
@@ -14,10 +17,21 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CommunityChatActivity extends AppCompatActivity {
 
+    Button btnCreatePost;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community_chat);
 
+        btnCreatePost = findViewById(R.id.btnCommChatCreatePost);
+
+        btnCreatePost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CommunityChatActivity.this, CreateCommunityChatPost.class);
+                startActivity(intent);
+            }
+        });
     }
 }
